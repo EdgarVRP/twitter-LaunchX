4bb9246cbbabe2732496e1daa42b060225279c1f
EdgarVRP <edgarvrp22@gmail.com>
2022-08-02 09:01:28 -0500

    Requerimiento 2 Sprint 2

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -10,4 +10,14 @@ describe("Test for UserService",()=>{
         expect(user.name).toBe('Edgar')
         expect(user.bio).not.toBeUndefined()
     });
+    test('2. Get all user data in a list', ()=>{
+        //Por Codificar...
+        const user = UserService.create(1,'EdgarVRP','Edgar')
+        const userInfoInList=UserService.getInfo(user)
+        //Resultados Esperados
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe('EdgarVRP')
+        expect(userInfoInList[2]).toBe('Edgar')
+        expect(userInfoInList[3]).toBe('Sin bio')
+    });
  })
