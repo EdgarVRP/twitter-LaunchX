a36f5a9849222f6afcaf74ea73a3b73605dc8dd0
EdgarVRP <edgarvrp22@gmail.com>
2022-08-02 10:21:33 -0500

    Sprint 3 Req 2 y 3

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -8,7 +8,16 @@ describe("Test for UserView",()=>{
         //Resultados Esperados
         expect(result.error).toMatch(/payload no existe/)
     });
-    test('2. Return an error object when try to create a new user with missing properties', ()=>{
+
+    test('2. Return an error object when try to create a new user with invalid properties', ()=>{
+        //Por Codificar...
+        const payload={username:null,name:12,id:'id'}
+        const result=UserView.createUser(payload)
+        //Resultados Esperados
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+    });
+
+    test('3. Return an error object when try to create a new user with missing properties', ()=>{
         //Por Codificar...
         const payload={username:'Username'}
         const result=UserView.createUser(payload)
